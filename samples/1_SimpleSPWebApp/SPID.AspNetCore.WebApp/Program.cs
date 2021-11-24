668b2ff4946a5b86056f5f49cb6288ab5daddbc5
Daniele Giallonardo <daniele.giallonardo@asfweb.it>
2021-11-24 12:33:36 +0100

    Modifica startup.cs

--- samples/1_SimpleSPWebApp/SPID.AspNetCore.WebApp/Program.cs
+++ samples/1_SimpleSPWebApp/SPID.AspNetCore.WebApp/Program.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using System.IO.Compression;
-using System.IO;
-using System.Text;
-using System;
-using System.Web;
-using SPID.AspNetCore.Authentication.Saml;
 
 namespace SPID.AspNetCore.WebApp
 {
-    public class Program
+	public class Program
 	{
         public static void Main(string[] args)
 		{
--- samples/1_SimpleSPWebApp/SPID.AspNetCore.WebApp/Startup.cs
+++ samples/1_SimpleSPWebApp/SPID.AspNetCore.WebApp/Startup.cs
@@ -48,16 +48,8 @@ namespace SPID.AspNetCore.WebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            app.UseDeveloperExceptionPage();
+            app.UseHsts();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
